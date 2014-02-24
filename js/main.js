abd8f839a4922ea08965f7e468c7ea841d89d68e
Samer Albahra <salbahra@gmail.com>
2014-02-23 20:13:20 -0600

    Splash: Fix problem inserting splash screen into page

--- js/main.js
+++ js/main.js
@@ -18,12 +18,12 @@ $(document).ready(function () {
         (function(){
             var p, l, r = window.devicePixelRatio, h = window.screen.height;
             if (navigator.platform === "iPad") {
-                    p = r === 2 ? "img/startup-tablet-portrait-retina.png" : "img/startup-tablet-portrait.png";
-                    l = r === 2 ? "img/startup-tablet-landscape-retina.png" : "img/startup-tablet-landscape.png";
-                    document.write('<link rel="apple-touch-startup-image" href="'+l+'" media="screen and (orientation: landscape)"><link rel="apple-touch-startup-image" href="'+p+'" media="screen and (orientation: portrait)">');
+                p = r === 2 ? "img/startup-tablet-portrait-retina.png" : "img/startup-tablet-portrait.png";
+                l = r === 2 ? "img/startup-tablet-landscape-retina.png" : "img/startup-tablet-landscape.png";
+                $('<link rel="apple-touch-startup-image" href="'+l+'" media="screen and (orientation: landscape)"><link rel="apple-touch-startup-image" href="'+p+'" media="screen and (orientation: portrait)">').appendTo("body");
             } else {
-                    p = r === 2 ? (h === 568 ? "img/startup-iphone5-retina.png" : "img/startup-retina.png") : "img/startup.png";
-                    document.write('<link rel="apple-touch-startup-image" href="'+p+'">');
+                p = r === 2 ? (h === 568 ? "img/startup-iphone5-retina.png" : "img/startup-retina.png") : "img/startup.png";
+                $('<link rel="apple-touch-startup-image" href="'+p+'">').appendTo("body");
             }
         })()
     }
