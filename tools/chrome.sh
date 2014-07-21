0551fae7e99069db0d70ea1c35d493e2088a5653
Samer Albahra <salbahra@gmail.com>
2014-07-20 23:04:46 -0500

    Base: Add script to build Chrome and Firefox OS builds

---
+++ tools/chrome.sh
@@ -0,0 +1,1 @@
+zip -r build/chrome/com.albahra.sprinklers.zip css img index.html js locale manifest.json res/chrome
---
+++ tools/firefox.sh
@@ -0,0 +1,1 @@
+zip -r build/firefoxos/com.albahra.sprinklers.zip css img index.html js locale manifest.webapp res/firefoxos
