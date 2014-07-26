22e78a0d08cccdc986b188325c6c7e5fae05ac0b
Samer Albahra <salbahra@gmail.com>
2014-07-25 19:19:13 -0500

    Mac: First version supporting native OS X builds
    Used for Mac App Store release

---
+++ source/osx/Resources/AppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.h
+//  Sprinklers
+//
+//  Created by Samer Albahra on 7/24/14.
+//  Copyright (c) 2014 Samer Albahra. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+#import <WebKit/WebPreferences.h>
+
+@interface AppDelegate : NSObject  {
+	NSWindow *window;
+	IBOutlet WebView *webView;
+}
+
+@property (strong) IBOutlet NSWindow *window;
+@property (nonatomic, retain) IBOutlet WebView *webView;
+
+@end
+
+@interface WebPreferences (WebPreferencesPrivate)
+- (void)_setLocalStorageDatabasePath:(NSString *)path;
+- (void) setLocalStorageEnabled: (BOOL) localStorageEnabled;
+@end
