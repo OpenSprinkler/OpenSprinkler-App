f49a9a9cde4fc8fd49b874031e2d02c4b338ec17
Samer Albahra <salbahra@gmail.com>
2014-08-02 15:14:46 -0500

    Base: Update Blackberry 10 build script

--- tasks/blackberry10.sh
+++ tasks/blackberry10.sh
@@ -1,6 +1,2 @@
-BUILD_DIR="/Users/salbahra/WebWorks Projects/sprinklers"
-
-( cd "$BUILD_DIR/www/"; rm -r * )
-cp -r www/* "$BUILD_DIR/www/"
-( cd "$BUILD_DIR"; cordova build --release )
-zip build/blackberry10/com.albahra.sprinklers.zip "$BUILD_DIR/platforms/blackberry10/build/device/bb10app.bar"
+cordova build --release
+zip build/blackberry10/com.albahra.sprinklers.zip platforms/blackberry10/build/device/bb10app.bar
