fcf13aba0cb418f138191ae8bf5f260f28125e02
Samer Albahra <salbahra@gmail.com>
2014-08-15 17:52:28 -0500

    Base: Fix bump script

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,7 +71,7 @@ module.exports = function(grunt) {
       },
       pushBump: {
           command: [
-            "git add www/index.html source/osx/Resources/Sprinklers-Info.plist www/config.xml manifest.json manifest.webapp package.json",
+            "git add www/js/main.js source/osx/Resources/Sprinklers-Info.plist www/config.xml manifest.json manifest.webapp package.json",
             "git commit -m 'Base: Increment version number'",
             "git push"
           ].join("&&")
