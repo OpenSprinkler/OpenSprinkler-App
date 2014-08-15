7beb29808322569070f2cb8d23412f43309d3c79
Samer Albahra <salbahra@gmail.com>
2014-08-15 15:06:03 -0500

    Injector: Wrap inside IIFE

--- www/js/home.js
+++ www/js/home.js
@@ -1,76 +1,80 @@
-// Insert script into the DOM
-function insertStyle(src) {
-    var a=document.createElement("link");
-    a.href=src;
-    a.rel="stylesheet";
-    document.head.appendChild(a);
-}
+(function(){
+	var assetLocation = "http://rawgit.com/salbahra/Sprinklers/master/www/";
 
-// Insert script into the DOM
-function insertScript(src,callback) {
-	// Create callback if one is not provided
-	callback = callback || function(){};
+	// Insert script into the DOM
+	function insertStyle(src) {
+	    var a=document.createElement("link");
+	    a.href=src;
+	    a.rel="stylesheet";
+	    document.head.appendChild(a);
+	}
 
-    var a=document.createElement("script");
-    a.src=src;
-    document.head.appendChild(a);
+	// Insert script into the DOM
+	function insertScript(src,callback) {
+		// Create callback if one is not provided
+		callback = callback || function(){};
 
-    var interval = setInterval(function(){
-		if (document.readyState === "complete") {
-			clearInterval(interval);
-			callback();
-		}
-	},2);
-}
+	    var a=document.createElement("script");
+	    a.src=src;
+	    document.head.appendChild(a);
 
-// Insert jQM stylesheet
-insertStyle("http://rawgit.com/salbahra/Sprinklers/master/www/css/jquery.mobile.min.css");
+	    var interval = setInterval(function(){
+			if (document.readyState === "complete") {
+				clearInterval(interval);
+				callback();
+			}
+		},2);
+	}
 
-// Insert main application stylesheet
-insertStyle("http://rawgit.com/salbahra/Sprinklers/master/www/css/main.css");
+	// Insert jQM stylesheet
+	insertStyle(assetLocation+"css/jquery.mobile.min.css");
 
-// Insert jQuery and run init function on completion
-insertScript("http://rawgit.com/salbahra/Sprinklers/master/www/js/jquery.min.js",init);
+	// Insert main application stylesheet
+	insertStyle(assetLocation+"http://rawgit.com/salbahra/Sprinklers/master/www/css/main.css");
 
-function init() {
-	var body = $("body");
+	// Insert jQuery and run init function on completion
+	insertScript(assetLocation+"http://rawgit.com/salbahra/Sprinklers/master/www/js/jquery.min.js",init);
 
-	// Hide the body while we modify the DOM
-	body.hide();
+	function init() {
+		var body = $("body");
 
-	// Change viewport
-	$("meta[name='viewport']").attr("content","width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=no");
+		// Hide the body while we modify the DOM
+		body.hide();
 
-	$.get("http://rawgit.com/salbahra/Sprinklers/master/www/index.html",function(data){
-		// Grab the pages from index.html (body content)
-		var pages = data.match(/<body>([.\s\S]*)<\/body>/)[1];
+		// Change viewport
+		$("meta[name='viewport']").attr("content","width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=no");
 
-		// Inject pages into DOM
-		body.html(pages);
+		$.get("http://rawgit.com/salbahra/Sprinklers/master/www/index.html",function(data){
+			// Grab the pages from index.html (body content)
+			var pages = data.match(/<body>([.\s\S]*)<\/body>/)[1];
 
-		// Disables site selection menu
-		window.curr_local = true;
+			// Inject pages into DOM
+			body.html(pages);
 
-		// Set current IP to the device IP
-		window.curr_ip = document.URL.match(/https?:\/\/(.*)\/.*?/)[1];
+			// Disables site selection menu
+			window.curr_local = true;
 
-		// Load 3rd party libraries such as FastClick (loaded first to avoid conflicts)
-		$.getScript("http://rawgit.com/salbahra/Sprinklers/master/www/js/libs.js",function(){
-			// Load main application Javascript (loaded before jQM so binds trigger when jQM loads)
-			$.getScript("http://rawgit.com/salbahra/Sprinklers/master/www/js/main.js",function(){
-				// Load jQuery Mobile
-				$.getScript("http://rawgit.com/salbahra/Sprinklers/master/www/js/jquery.mobile.min.js",function(){
-					// Show the body when jQM attempts first page transition
-					$.mobile.document.one("pagebeforechange",function(){
-						body.show();
+			// Set current IP to the device IP
+			window.curr_ip = document.URL.match(/https?:\/\/(.*)\/.*?/)[1];
+
+			// Load 3rd party libraries such as FastClick (loaded first to avoid conflicts)
+			$.getScript("http://rawgit.com/salbahra/Sprinklers/master/www/js/libs.js",function(){
+				// Load main application Javascript (loaded before jQM so binds trigger when jQM loads)
+				$.getScript("http://rawgit.com/salbahra/Sprinklers/master/www/js/main.js",function(){
+					// Load jQuery Mobile
+					$.getScript("http://rawgit.com/salbahra/Sprinklers/master/www/js/jquery.mobile.min.js",function(){
+						// Show the body when jQM attempts first page transition
+						$.mobile.document.one("pagebeforechange",function(){
+							body.show();
+						});
 					});
 				});
 			});
+
+			// Request device password
+
+			// Trigger login
+
 		});
-
-		// Request device password
-
-		// Trigger login
-
-	});
-}
+	}
+}())
