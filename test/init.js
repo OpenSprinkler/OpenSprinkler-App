c61f1051e2dec4f38c8ae636933c4ab43b2f981b
Samer Albahra <salbahra@gmail.com>
2014-08-23 20:00:32 -0500

    Base: Initial support for automated testing

---
+++ test/init.js
@@ -0,0 +1,11 @@
+var assert = chai.assert;
+
+describe("Intial Setup", function(){
+	describe("Check if storage is defined", function(){
+		it("Should be defined", function(){
+			assert.equal("function", typeof storage.get);
+			assert.equal("function", typeof storage.set);
+			assert.equal("function", typeof storage.remove);
+		})
+	})
+})
