c55d77fa356212b23a2c3c98f6f265a35cc2ce1c
Samer Albahra <salbahra@gmail.com>
2015-01-17 11:22:59 -0600

    Base: Increment version number

--- www/js/main.js
+++ www/js/main.js
@@ -6816,7 +6816,7 @@ function show_about() {
                     "</li>" +
                 "</ul>" +
                 "<p class='smaller'>" +
-                    _("App Version")+": 1.2.6" +
+                    _("App Version")+": 1.2.7" +
                     (typeof controller.options.hwv !== "undefined" ? "<br>"+_("Hardware Version")+": v"+getHWVersion() : "") +
                     "<br>"+_("Firmware")+": "+getOSVersion() +
                 "</p>" +
