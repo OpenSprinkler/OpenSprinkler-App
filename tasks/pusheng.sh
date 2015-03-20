ad3ac40805c0a86911f770c00b5da2d20b076596
Samer Albahra <salbahra@gmail.com>
2015-03-19 21:28:36 -0500

    Grunt: Move tasks into Gruntfile

--- tasks/pusheng.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-xgettext --keyword=_ --output=- www/js/main.js --omit-header --force-po --language="Python" | sed '/^\#/d' > .msgjs
-sed -E 's/data-translate=(".*")/_(\1)/g' www/index.htm | xgettext --keyword=_ --output=- --language="Python" --omit-header --force-po - | sed '/^\#/d' > .msghtml
-msgcat .msgjs .msghtml > www/locale/messages_en.po
-rm .msgjs .msghtml
-git add www/locale/messages_en.po
-git commit -m "Localization: Update English strings"
-git push
