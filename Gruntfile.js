acff2b6ee6755a9e8e2f02591665dd9dcc852038
Samer Albahra <salbahra@gmail.com>
2015-03-19 21:41:15 -0500

    Grunt: Fix updateLang task

--- Gruntfile.js
+++ Gruntfile.js
@@ -157,7 +157,10 @@ module.exports = function(grunt) {
 					"curl --user <%= secrets.getLocalization.username %>:<%= secrets.getLocalization.password %> https://api.getlocalization.com/Sprinklers/api/translations/zip/ -o langs.zip",
 					"unzip langs.zip",
 					"rm langs.zip",
-					"find . -type f -maxdepth 1 -iname 'messages_*.po' -print0 | while IFS=read -r -d $'\\0' line; do file=(${line//_/ }); lang=${file[1]}; file=(${lang//-/ }); lang=${file[0]}; file=(${lang//./ }); lang=${file[0]}; mv $line messages.po; po2json -p messages.po > 'www/locale/$lang.js'; rm messages.po; done"
+					"find . -type f -maxdepth 1 -iname 'messages_*.po' -print0 | while IFS= read -r -d $'\\0' line; do file=(${line//_/ }); lang=${file[1]}; file=(${lang//-/ }); lang=${file[0]}; file=(${lang//./ }); lang=${file[0]}; mv \"$line\" messages.po; po2json -p messages.po > \"www/locale/$lang.js\"; rm messages.po; done",
+					"git add www/locale",
+					"git commit -m 'Localization: Update languages from getlocalization.com'",
+					"git push"
 				].join("&&")
 			},
 			symres: {
