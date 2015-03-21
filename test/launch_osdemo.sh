1df9b04289a4153f2ddb2f8267414344f4623615
Samer Albahra <salbahra@gmail.com>
2015-03-21 13:08:14 -0500

    Base: Minor clean up

--- test/launch_osdemo.sh
+++ test/launch_osdemo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd build
-git clone https://github.com/opensprinkler/opensprinklergen2
+git clone -b fix-nonblock https://github.com/opensprinkler/opensprinklergen2
 cd opensprinklergen2
 ./build_demo.sh
 ./OpenSprinkler &
