c4fc15821102ca50d8268126ac1952ddd6c36b77
Samer Albahra <salbahra@gmail.com>
2015-03-21 20:30:19 -0500

    Tests: Allow firmware software to start/stop

--- test/launch_ospi.sh
+++ test/launch_ospi.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 cd build/firmware
-git clone https://github.com/dan-in-ca/ospi
-cd ospi
-python ospi.py &
+
+if [ $1 == "start" ]; then
+	git clone https://github.com/dan-in-ca/ospi
+	cd ospi
+	python ospi.py &
+	echo $! > pid
+else
+	kill -9 `cat ospi/pid`
+	rm ospi/pid
+fi
