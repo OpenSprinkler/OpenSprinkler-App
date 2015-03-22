69fa3c9c996ba132eb9d26f5bf60b94b9f517b50
Samer Albahra <salbahra@gmail.com>
2015-03-21 20:30:19 -0500

    Tests: Allow firmware software to start/stop

--- test/launch_osdemo.sh
+++ test/launch_osdemo.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 cd build/firmware
-git clone -b fix-nonblock https://github.com/opensprinkler/opensprinklergen2 unified
-cd unified
-./build_demo.sh
-./OpenSprinkler &
+
+if [ $1 == "start" ]; then
+	git clone https://github.com/opensprinkler/opensprinklergen2 unified
+	cd unified
+	./build_demo.sh
+	./OpenSprinkler &
+	echo $! > pid
+else
+	kill -9 `cat unified/pid`
+	rm unified/pid
+fi
