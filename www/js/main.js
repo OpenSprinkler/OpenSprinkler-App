497566dbcbf2638b5e2a8a868a9a2006103262c9
f613c71b3ed9988f9784f8b94f3d675053e7e9d3 <salbahra@gmail.com>
2015-06-23 17:24:00 -0500

    Base: Increment version number

--- www/js/main.js
+++ www/js/main.js
@@ -8047,7 +8047,7 @@ var showAbout = ( function() {
                     "</li>" +
                 "</ul>" +
                 "<p class='smaller'>" +
-                    _( "App Version" ) + ": 1.4.4" +
+                    _( "App Version" ) + ": 1.4.5" +
                     "<br>" + _( "Firmware" ) + ": <span class='firmware'></span>" +
                     "<br><span class='hardwareLabel'>" + _( "Hardware Version" ) + ":</span> <span class='hardware'></span>" +
                 "</p>" +
