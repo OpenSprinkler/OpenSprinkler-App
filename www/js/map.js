ad6cde991bf3fc72e799e2ed15b4d4e835f91e56
f613c71b3ed9988f9784f8b94f3d675053e7e9d3 <salbahra@gmail.com>
2015-08-30 03:23:35 -0500

    Options: Improve maps control buttons

--- www/js/map.js
+++ www/js/map.js
@@ -32,14 +32,15 @@ google.maps = google.maps || {};
     google.maps.Load = function( apiLoad ) {
         delete google.maps.Load;
 
-        apiLoad([0.009999999776482582,[[["https://mts0.googleapis.com/vt?lyrs=m@292000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.googleapis.com/vt?lyrs=m@292000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"m@292000000",["https://mts0.google.com/vt?lyrs=m@292000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.google.com/vt?lyrs=m@292000000\u0026src=api\u0026hl=en-US\u0026"]],[["https://khms0.googleapis.com/kh?v=166\u0026hl=en-US\u0026","https://khms1.googleapis.com/kh?v=166\u0026hl=en-US\u0026"],null,null,null,1,"166",["https://khms0.google.com/kh?v=166\u0026hl=en-US\u0026","https://khms1.google.com/kh?v=166\u0026hl=en-US\u0026"]],[["https://mts0.googleapis.com/vt?lyrs=h@292000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.googleapis.com/vt?lyrs=h@292000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"h@292000000",["https://mts0.google.com/vt?lyrs=h@292000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.google.com/vt?lyrs=h@292000000\u0026src=api\u0026hl=en-US\u0026"]],[["https://mts0.googleapis.com/vt?lyrs=t@132,r@292000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.googleapis.com/vt?lyrs=t@132,r@292000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"t@132,r@292000000",["https://mts0.google.com/vt?lyrs=t@132,r@292000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.google.com/vt?lyrs=t@132,r@292000000\u0026src=api\u0026hl=en-US\u0026"]],null,null,[["https://cbks0.googleapis.com/cbk?","https://cbks1.googleapis.com/cbk?"]],[["https://khms0.googleapis.com/kh?v=84\u0026hl=en-US\u0026","https://khms1.googleapis.com/kh?v=84\u0026hl=en-US\u0026"],null,null,null,null,"84",["https://khms0.google.com/kh?v=84\u0026hl=en-US\u0026","https://khms1.google.com/kh?v=84\u0026hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt?hl=en-US\u0026","https://mts1.googleapis.com/mapslt?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/ft?hl=en-US\u0026"]],[["https://mts0.googleapis.com/vt?hl=en-US\u0026","https://mts1.googleapis.com/vt?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/loom?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/loom?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt?hl=en-US\u0026","https://mts1.googleapis.com/mapslt?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/ft?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/loom?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/loom?hl=en-US\u0026"]]],["en-US","US",null,0,null,null,"https://maps.gstatic.com/mapfiles/","https://csi.gstatic.com","https://maps.googleapis.com","https://maps.googleapis.com",null,"https://maps.google.com","https://csi.gstatic.com","https://maps.gstatic.com/maps-api-v3/api/images/"],["https://maps.gstatic.com/maps-api-v3/api/js/19/10","3.19.10"],[1958094959],1,null,null,null,null,null,"",null,null,1,"https://khms.googleapis.com/mz?v=166\u0026",null,"https://earthbuilder.googleapis.com","https://earthbuilder.googleapis.com",null,"https://mts.googleapis.com/vt/icon",[["https://mts0.googleapis.com/vt","https://mts1.googleapis.com/vt"],["https://mts0.googleapis.com/vt","https://mts1.googleapis.com/vt"],null,null,null,null,null,null,null,null,null,null,["https://mts0.google.com/vt","https://mts1.google.com/vt"],"/maps/vt",292000000,132],2,500,[null,"https://g0.gstatic.com/landmark/tour","https://g0.gstatic.com/landmark/config","","https://www.google.com/maps/preview/log204","","https://static.panoramio.com.storage.googleapis.com/photos/",["https://geo0.ggpht.com/cbk","https://geo1.ggpht.com/cbk","https://geo2.ggpht.com/cbk","https://geo3.ggpht.com/cbk"]],["https://www.google.com/maps/api/js/master?pb=!1m2!1u19!2s10!2sen-US!3sUS!4s19/10","https://www.google.com/maps/api/js/widget?pb=!1m2!1u19!2s10!2sen-US"],null,0,0], loadScriptTime);
+        apiLoad([0.009999999776482582,[[["https://mts0.googleapis.com/vt?lyrs=m@318000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.googleapis.com/vt?lyrs=m@318000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"m@318000000",["https://mts0.google.com/vt?lyrs=m@318000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.google.com/vt?lyrs=m@318000000\u0026src=api\u0026hl=en-US\u0026"]],[["https://khms0.googleapis.com/kh?v=182\u0026hl=en-US\u0026","https://khms1.googleapis.com/kh?v=182\u0026hl=en-US\u0026"],null,null,null,1,"182",["https://khms0.google.com/kh?v=182\u0026hl=en-US\u0026","https://khms1.google.com/kh?v=182\u0026hl=en-US\u0026"]],null,[["https://mts0.googleapis.com/vt?lyrs=t@132,r@318000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.googleapis.com/vt?lyrs=t@132,r@318000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"t@132,r@318000000",["https://mts0.google.com/vt?lyrs=t@132,r@318000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.google.com/vt?lyrs=t@132,r@318000000\u0026src=api\u0026hl=en-US\u0026"]],null,null,[["https://cbks0.googleapis.com/cbk?","https://cbks1.googleapis.com/cbk?"]],[["https://khms0.googleapis.com/kh?v=87\u0026hl=en-US\u0026","https://khms1.googleapis.com/kh?v=87\u0026hl=en-US\u0026"],null,null,null,null,"87",["https://khms0.google.com/kh?v=87\u0026hl=en-US\u0026","https://khms1.google.com/kh?v=87\u0026hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt?hl=en-US\u0026","https://mts1.googleapis.com/mapslt?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/ft?hl=en-US\u0026"]],[["https://mts0.googleapis.com/vt?hl=en-US\u0026","https://mts1.googleapis.com/vt?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/loom?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/loom?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt?hl=en-US\u0026","https://mts1.googleapis.com/mapslt?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/ft?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/loom?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/loom?hl=en-US\u0026"]]],["en-US","US",null,0,null,null,"https://maps.gstatic.com/mapfiles/","https://csi.gstatic.com","https://maps.googleapis.com","https://maps.googleapis.com",null,"https://maps.google.com","https://gg.google.com","https://maps.gstatic.com/maps-api-v3/api/images/","https://www.google.com/maps",0,"https://www.google.com"],["https://maps.googleapis.com/maps-api-v3/api/js/21/9b","3.21.9b"],[2575381618],1,null,null,null,null,null,"",["places"],null,1,"https://khms.googleapis.com/mz?v=182\u0026",null,"https://earthbuilder.googleapis.com","https://earthbuilder.googleapis.com",null,"https://mts.googleapis.com/vt/icon",[["https://mts0.googleapis.com/vt","https://mts1.googleapis.com/vt"],["https://mts0.googleapis.com/vt","https://mts1.googleapis.com/vt"],null,null,null,null,null,null,null,null,null,null,["https://mts0.google.com/vt","https://mts1.google.com/vt"],"/maps/vt",318000000,132],2,500,[null,"https://g0.gstatic.com/landmark/tour","https://g0.gstatic.com/landmark/config",null,"https://www.google.com/maps/preview/log204","","https://static.panoramio.com.storage.googleapis.com/photos/",["https://geo0.ggpht.com/cbk","https://geo1.ggpht.com/cbk","https://geo2.ggpht.com/cbk","https://geo3.ggpht.com/cbk"],"https://maps.googleapis.com/maps/api/js/GeoPhotoService.GetMetadata","https://maps.googleapis.com/maps/api/js/GeoPhotoService.SingleImageSearch"],["https://www.google.com/maps/api/js/master?pb=!1m2!1u21!2s9b!2sen-US!3sUS!4s21/9b","https://www.google.com/maps/api/js/widget?pb=!1m2!1u21!2s9b!2sen-US"],null,0,null,"/maps/api/js/ApplicationService.GetEntityDetails",0,null,null,[null,null,null,null,null,null,null,null,null,[0,0],[0,null,1,"0","0","U","0","0","0","0"]]], loadScriptTime);
     };
 
     var loadScriptTime = new Date().getTime();
 
     // jshint ignore:end
     // jscs:enable
-    getScript( "https://maps.gstatic.com/maps-api-v3/api/js/19/10/main.js" );
+    getScript( "https://maps.googleapis.com/maps-api-v3/api/js/21/9b/main.js" );
+    getScript( "https://maps.googleapis.com/maps-api-v3/api/js/21/9b/places.js" );
 } )();
 
 var markers = { airport: [], pws: [], orgin: [] },
@@ -47,6 +48,9 @@ var markers = { airport: [], pws: [], orgin: [] },
 
 // Handle select button for weather station selection
 document.addEventListener( "click", function( e ) {
+	if ( e.target.tagName !== "BUTTON" ) {
+		return;
+	}
     var classes = e.target.className.split( " " );
     if ( classes.indexOf( "submit" ) > -1 ) {
         window.top.postMessage( { WS: e.target.dataset.loc }, "*" );
@@ -68,17 +72,44 @@ function initialize() {
         map = new google.maps.Map( document.getElementById( "map_canvas" ), myOptions );
         infoWindow = new google.maps.InfoWindow();
 
+		var controlBox = document.getElementById( "pac-container" ),
+			jumpToCurrent = document.getElementById( "pac-current" ),
+			searchField = document.getElementById( "pac-input" ),
+			searchBox = new google.maps.places.SearchBox( searchField );
+
+		map.controls[ google.maps.ControlPosition.TOP_LEFT ].push( controlBox );
+
+		// Bias the SearchBox results towards current map's viewport.
+		map.addListener( "bounds_changed", function() {
+			searchBox.setBounds( map.getBounds() );
+		} );
+
+		searchBox.addListener( "places_changed", function() {
+		    var places = searchBox.getPlaces();
+		    if ( places.length === 0 ) {
+				return;
+			}
+
+            if ( droppedPin ) {
+                droppedPin.setMap( null );
+                droppedPin = null;
+            }
+            droppedPin = plotMarker( "orgin", { message: "Selected Location" }, places[ 0 ].geometry.location.lat(), places[ 0 ].geometry.location.lng() );
+		} );
+
         // If a current location is specified, show the current location button
         // The app uses -999, -999 when geolocation is not possible which is resolved to -90, 81
         if ( start.lat() !== -90 && start.lng() !== 81 ) {
-
-			// Create the DIV to hold the custom control and call the constructor
-			var locationDiv = document.createElement( "div" );
-			new locationControl( locationDiv, map );
-
-			locationDiv.index = 1;
-			map.controls[ google.maps.ControlPosition.TOP_CENTER ].push( locationDiv );
 			start = plotMarker( "orgin", { message: "Current Location" }, start.lat(), start.lng() );
+
+			// Bind the current location button
+			jumpToCurrent.addEventListener( "click", function() {
+				infoWindow.close();
+				map.setCenter( { lat: start.getPosition().lat(), lng: start.getPosition().lng() } );
+				google.maps.event.trigger( start, "click" );
+			} );
+
+			jumpToCurrent.style.display = "inline";
 		}
 
         // If a current location is specified, display and center it now
@@ -217,44 +248,6 @@ function createInfoWindow( type, data, latLon ) {
     } else {
         return "<div style='min-height:40px;text-align:center'>" + data.message + "<br><br><button class='submit' data-loc='" + latLon + "'>Submit</button></div>";
     }
-}
-
-/**
- * The CenterControl adds a control to the map that recenters the map on Chicago.
- * This constructor takes the control DIV as an argument.
- * @constructor
- */
-function locationControl( controlDiv ) {
-
-	// Set CSS for the control border
-	var controlUI = document.createElement( "div" );
-	controlUI.style.backgroundColor = "#fff";
-	controlUI.style.border = "2px solid #fff";
-	controlUI.style.borderRadius = "3px";
-	controlUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
-	controlUI.style.cursor = "pointer";
-	controlUI.style.marginBottom = "22px";
-	controlUI.style.textAlign = "center";
-	controlUI.title = "Click to move to current location";
-	controlDiv.appendChild( controlUI );
-
-	// Set CSS for the control interior
-	var controlText = document.createElement( "div" );
-	controlText.style.color = "rgb(25,25,25)";
-	controlText.style.fontFamily = "Roboto,Arial,sans-serif";
-	controlText.style.fontSize = "16px";
-	controlText.style.lineHeight = "38px";
-	controlText.style.paddingLeft = "5px";
-	controlText.style.paddingRight = "5px";
-	controlText.innerHTML = "Jump to Current Location";
-	controlUI.appendChild( controlText );
-
-	// Setup the click event listeners: simply set the map to Chicago.
-	controlUI.addEventListener( "click", function() {
-		infoWindow.close();
-		map.setCenter( { lat: start.getPosition().lat(), lng: start.getPosition().lng() } );
-		google.maps.event.trigger( start, "click" );
-	} );
 }
 
 function rad( x ) {
