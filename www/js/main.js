370da55a4fb814bf1064746622c4cdaf0d0f825e
Samer Albahra <salbahra@gmail.com>
2018-05-27 19:06:19 -0500

    Base: Increment version number

--- www/js/main.js
+++ www/js/main.js
@@ -9352,7 +9352,7 @@ var showAbout = ( function() {
                     "</li>" +
                 "</ul>" +
                 "<p class='smaller'>" +
-                    _( "App Version" ) + ": 1.7.5" +
+                    _( "App Version" ) + ": 1.7.6" +
                     "<br>" + _( "Firmware" ) + ": <span class='firmware'></span>" +
                     "<br><span class='hardwareLabel'>" + _( "Hardware Version" ) + ":</span> <span class='hardware'></span>" +
                 "</p>" +
