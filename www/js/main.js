9bade7d1ed4091932a6c7ccb8861b2075888b0b9
Samer Albahra <salbahra@gmail.com>
2019-07-16 21:15:16 -0700

    Bump to 2.0.0

--- www/js/main.js
+++ www/js/main.js
@@ -9152,7 +9152,7 @@ var showAbout = ( function() {
 					"</li>" +
 				"</ul>" +
 				"<p class='smaller'>" +
-					_( "App Version" ) + ": 1.8.8" +
+					_( "App Version" ) + ": 2.0.0" +
 					"<br>" + _( "Firmware" ) + ": <span class='firmware'></span>" +
 					"<br><span class='hardwareLabel'>" + _( "Hardware Version" ) + ":</span> <span class='hardware'></span>" +
 				"</p>" +
