22538722a365fbb42426aadd9b6f361a15403322
Samer Albahra <salbahra@gmail.com>
2022-12-14 13:28:52 -0500

    Fix bump level

--- www/js/main.js
+++ www/js/main.js
@@ -10358,7 +10358,7 @@ var showAbout = ( function() {
 					"</li>" +
 				"</ul>" +
 				"<p class='smaller'>" +
-					_( "App Version" ) + ": 2.3.5" +
+					_( "App Version" ) + ": 2.3.0" +
 					"<br>" + _( "Firmware" ) + ": <span class='firmware'></span>" +
 					"<br><span class='hardwareLabel'>" + _( "Hardware Version" ) + ":</span> <span class='hardware'></span>" +
 				"</p>" +
