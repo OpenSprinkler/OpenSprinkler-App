56fbb516a44648235c4349b9d633c8f9f4ac04e6
Benjamin Lehr <b.lehr@trio-group.de>
2023-02-05 10:44:29 +0100

    add chart.js cdn link and add dummy chart

--- www/js/main.js
+++ www/js/main.js
@@ -5524,14 +5524,13 @@ var showCharts = ( function() {
 			"<div class='ui-content' role='main'>" +
 			"<ul data-role='listview' data-inset='true'>" +
 			"<h1>Charts Test</h1>" +
+			"<canvas id='myChart'></canvas>" +
 			"</div>" +
 			"</div>" ),
 		chart;
 
 	function begin() {
 		chart = null;
-
-		// page.find( ".firmware" ).text( getOSVersion() + getOSMinorVersion() );
 
 		page.one( "pagehide", function() {
 			page.detach();
@@ -5549,6 +5548,56 @@ var showCharts = ( function() {
 
 		$( "#about" ).remove();
 		$.mobile.pageContainer.append( page );
+
+
+		// set chart js labels and datasets
+		var labels = [1,2,3,4,5,6,7];
+		var data = {
+			labels: labels,
+			datasets: [{
+				label: 'My First Dataset',
+				data: [65, 59, 80, 81, 56, 55, 40],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(255, 159, 64, 0.2)',
+					'rgba(255, 205, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(201, 203, 207, 0.2)'
+				],
+				borderColor: [
+					'rgb(255, 99, 132)',
+					'rgb(255, 159, 64)',
+					'rgb(255, 205, 86)',
+					'rgb(75, 192, 192)',
+					'rgb(54, 162, 235)',
+					'rgb(153, 102, 255)',
+					'rgb(201, 203, 207)'
+				],
+				borderWidth: 1
+			}]
+		};
+
+
+
+		// var ctx = page.find("myChart");
+		var ctx = document.getElementById('myChart');
+
+		new Chart(ctx, {
+			type: 'bar',
+			data: data,
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		});
+
+
+
 	}
 
 	return begin;
