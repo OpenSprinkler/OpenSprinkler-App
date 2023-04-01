0663f688f567b72b76524b40b957d403bdfe3090
Samer Albahra <salbahra@gmail.com>
2023-04-01 14:31:24 -0400

    Update version

--- www/js/main.js
+++ www/js/main.js
@@ -10422,7 +10422,7 @@ var showAbout = ( function() {
 					"</li>" +
 				"</ul>" +
 				"<p class='smaller'>" +
-					_( "App Version" ) + ": 2.3.0" +
+					_( "App Version" ) + ": 2.3.1" +
 					"<br>" + _( "Firmware" ) + ": <span class='firmware'></span>" +
 					"<br><span class='hardwareLabel'>" + _( "Hardware Version" ) + ":</span> <span class='hardware'></span>" +
 				"</p>" +
