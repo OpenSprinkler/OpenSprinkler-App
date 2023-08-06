1530a7127199dfa48abcad620b1772ac574f4cfe
Samer Albahra <salbahra@gmail.com>
2023-08-05 20:03:41 -0400

    Add missing tx push command

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,6 +129,7 @@
 						"sed -E 's/data-translate=(\".*\")/_(\\1)/g' www/index.html | xgettext --keyword=_ --output=- --language='Python' --omit-header --force-po - | sed '/^\#/d' > .msghtml",
 						"msgcat .msgjs .msghtml > www/locale/messages_en.po",
 						"rm .msgjs .msghtml",
+						"tx push",
 						"git add www/locale/messages_en.po",
 						"git diff-index --quiet HEAD || git commit -m 'Localization: Update English strings'",
 						"git push"
