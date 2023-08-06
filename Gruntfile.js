f9ba53e95d74a1367b2a1bb85c19bcd2525ed7e4
Samer Albahra <salbahra@gmail.com>
2023-08-06 17:08:01 -0400

    Fix clean step for pushFW

--- Gruntfile.js
+++ Gruntfile.js
@@ -222,8 +222,8 @@
 		grunt.registerTask( "test", [ "default", "blanket_mocha" ] );
 		grunt.registerTask( "updateLang", [ "shell:updateLang" ] );
 		grunt.registerTask( "pushEng", [ "shell:pushEng" ] );
-		grunt.registerTask( "pushFW", [ "compress:makeFW", "shell:updateUI", "clean:makeFW" ] );
-		grunt.registerTask( "pushBetaFW", [ "compress:makeFW", "shell:updateBetaUI", "clean:makeFW" ] );
+		grunt.registerTask( "pushFW", [ "compress:makeFW", "shell:updateUI", "clean:pushFW" ] );
+		grunt.registerTask( "pushBetaFW", [ "compress:makeFW", "shell:updateBetaUI", "clean:pushFW" ] );
 		grunt.registerTask( "build", [ "default", "shell:symres", "pushFW", "clean:symres" ] );
 		grunt.registerTask( "bump", [ "default", "replace:about", "replace:cordova", "replace:manifests", "shell:pushBump" ] );
 	};
