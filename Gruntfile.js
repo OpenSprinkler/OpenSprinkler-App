66399d49b75e19ac18eff444dfbbeb1b4dfe0fa3
Samer Albahra <salbahra@gmail.com>
2023-08-05 20:00:12 -0400

    Add analog sensor file for string extraction

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,7 +125,7 @@
 				},
 				pushEng: {
 					command: [
-						"xgettext --keyword=_ --output=- www/js/main.js --omit-header --force-po --from-code=UTF-8 --language='Python' | sed '/^\#/d' > .msgjs",
+						"xgettext --keyword=_ --output=- www/js/main.js www/js/analog.js --omit-header --force-po --from-code=UTF-8 --language='Python' | sed '/^\#/d' > .msgjs",
 						"sed -E 's/data-translate=(\".*\")/_(\\1)/g' www/index.html | xgettext --keyword=_ --output=- --language='Python' --omit-header --force-po - | sed '/^\#/d' > .msghtml",
 						"msgcat .msgjs .msghtml > www/locale/messages_en.po",
 						"rm .msgjs .msghtml",
