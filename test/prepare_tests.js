fd7f2f275e3abf3e2093bd61b59cc518f66cc876
Samer Albahra <salbahra@gmail.com>
2024-11-27 07:53:41 -0500

    Minor fix

--- test/prepare_tests.js
+++ test/prepare_tests.js
@@ -1,15 +1,15 @@
-describe('prepare_tests', function() {
-	before(function() {
-		return new Promise(function (resolve) {
-			$.ajax({
-				url: "/base/www/index.html",
-				cache: true,
-				type: "GET"
-			}).done(function (index) {
-				var body = index.match(/<body>([.\s\S]*)<\/body>/)[1];
-				$("body").prepend(body);
-				resolve();
-			});
-		} );
-	})
+/* eslint-disable */
+
+before(function () {
+	return new Promise(function (resolve) {
+		$.ajax({
+			url: "/base/www/index.html",
+			cache: true,
+			type: "GET"
+		}).done(function (index) {
+			var body = index.match(/<body>([.\s\S]*)<\/body>/)[1];
+			$("body").prepend(body);
+			resolve();
+		});
+	});
 });
