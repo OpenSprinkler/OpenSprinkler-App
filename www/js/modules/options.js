37025c67102ff9056096016157775d092bf4dfe6
Ray <rayshobbyshop@gmail.com>
2025-08-27 10:38:39 -0400

    minor wording change

--- www/js/modules/options.js
+++ www/js/modules/options.js
@@ -843,7 +843,7 @@ OSApp.Options.showOptions = function( expandItem ) {
 	if ( OSApp.Firmware.checkOSVersion( 2213 ) && typeof OSApp.currentSession.controller.options.imin !== "undefined" ) {
 		list += "<div class='ui-field-contain'><label for='imin'>" + OSApp.Language._( "Undercurrent Threshold" ) +
 			"<button data-helptext='" +
-				OSApp.Language._( "If the current draw falls below this threshold when a station runs, a low-current fault notification is triggered. Set to 0 to disable this feature." ) +
+				OSApp.Language._( "If the current draw falls below this threshold when a station finishes running, a low-current fault notification is triggered. Set to 0 to disable this feature." ) +
 			"' class='help-icon btn-no-border ui-btn ui-icon-info ui-btn-icon-notext'></button></label>" +
 			"<input type='range' id='imin' min='0' max='1000' step='10' data-highlight='true' value='" + ( OSApp.currentSession.controller.options.imin ) + "'></div>";
 	}
