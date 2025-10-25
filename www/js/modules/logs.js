cccdc588663c762613b0fca2728f1ef7e25abef0
Ray Wang <rayshobbyshop@gmail.com>
2025-10-25 10:11:45 -0400

    fix log display issue of rain sensor and rain delay events

--- www/js/modules/logs.js
+++ www/js/modules/logs.js
@@ -76,9 +76,9 @@ OSApp.Logs.displayPage = function() {
 			}
 
 			$.each( data, function() {
-                                var station = this[ 1 ],
-                                        duration = parseInt( this[ 2 ] ),
-                                        flowRate = ( typeof this[ 4 ] !== "undefined" ) ? OSApp.Utils.flowRateToVolume( parseFloat( this[ 4 ] ) ) : null;
+				var station = this[ 1 ],
+					duration = parseInt( this[ 2 ] ),
+					flowRate = ( typeof this[ 4 ] !== "undefined" ) ? OSApp.Utils.flowRateToVolume( parseFloat( this[ 4 ] ) ) : null;
 
 				// Adjust for negative watering time firmware bug
 				if ( duration < 0 ) {
@@ -114,15 +114,15 @@ OSApp.Logs.displayPage = function() {
 
 				if ( type === "table" ) {
 					switch ( grouping ) {
-                                                case "station":
-                                                        var stationItem = [ utc, OSApp.Dates.dhms2str( OSApp.Dates.sec2dhms( duration ) ), station, new Date( utc.getTime() + ( duration * 1000 ) ), flowRate ];
-                                                        sortedData[ station ].push( stationItem );
-                                                        break;
-                                                case "day":
-                                                        var day = Math.floor( date.getTime() / 1000 / 60 / 60 / 24 ),
-                                                                item = [ utc, OSApp.Dates.dhms2str( OSApp.Dates.sec2dhms( duration ) ), station, new Date( utc.getTime() + ( duration * 1000 ) ), flowRate ];
+						case "station":
+							var stationItem = [ utc, OSApp.Dates.dhms2str( OSApp.Dates.sec2dhms( duration ) ), station, new Date( utc.getTime() + ( duration * 1000 ) ), flowRate ];
+							sortedData[ station ].push( stationItem );
+							break;
+						case "day":
+							var day = Math.floor( date.getTime() / 1000 / 60 / 60 / 24 ),
+								item = [ utc, OSApp.Dates.dhms2str( OSApp.Dates.sec2dhms( duration ) ), station, new Date( utc.getTime() + ( duration * 1000 ) ), flowRate ];
 
-                                                        // Item structure: [startDate, runtime, station, endDate, flowRate]
+							// Item structure: [startDate, runtime, station, endDate, flowRate]
 
 							if ( typeof sortedData[ day ] !== "object" ) {
 								sortedData[ day ] = [ item ];
@@ -349,13 +349,13 @@ OSApp.Logs.displayPage = function() {
 				wlSorted = extraData[ 0 ],
 				flSorted = extraData[ 1 ],
 				stats = extraData[ 2 ],
-                                tableHeader = "<table class=\"table-logs-datatables\"><thead><tr>" +
-                                        "<th data-priority='1'>" + OSApp.Language._( "Station" ) + "</th>" +
-                                        "<th data-priority='2'>" + OSApp.Language._( "Runtime" ) + "</th>" +
-                                        "<th data-priority='3'>" + OSApp.Language._( "Start Time" ) + "</th>" +
-                                        "<th data-priority='4'>" + OSApp.Language._( "End Time" ) + "</th>" +
-                                        "<th data-priority='5'>" + OSApp.Language._( "Flow Rate" ) + "</th>" +
-                                        "</tr></thead><tbody>",
+				tableHeader = "<table class=\"table-logs-datatables\"><thead><tr>" +
+					"<th data-priority='1'>" + OSApp.Language._( "Station" ) + "</th>" +
+					"<th data-priority='2'>" + OSApp.Language._( "Runtime" ) + "</th>" +
+					"<th data-priority='3'>" + OSApp.Language._( "Start Time" ) + "</th>" +
+					"<th data-priority='4'>" + OSApp.Language._( "End Time" ) + "</th>" +
+					"<th data-priority='5'>" + OSApp.Language._( "Flow Rate" ) + "</th>" +
+					"</tr></thead><tbody>",
 				html = showStats( stats ) + "<div data-role='collapsible-set' data-inset='true' data-theme='b' data-collapsed-icon='arrow-d' data-expanded-icon='arrow-u'>",
 				i = 0,
 				group, ct, k;
@@ -419,20 +419,20 @@ OSApp.Logs.displayPage = function() {
 
 					for ( k = 0; k < sortedData[ group ].length; k++ ) {
 						var sid = ( grouping === 'station' ) ? group  : sortedData[group][k][2];
-						var stationName = OSApp.Stations.getName(sid);
+						var stationName = stations[sid];
 						var runTime = sortedData[ group ][ k ][ 1 ];
 						var startTime = formatTime( sortedData[ group ][ k ][ 0 ], grouping ) ;
-                                                var endTime = formatTime( sortedData[ group ][ k ][ 3 ], grouping );
-                                                var fRate = sortedData[ group ][ k ][ 4 ];
-                                                var flowDisplay = ( typeof fRate === "number" ) ? fRate.toFixed( 2 ) + " L/min" : "";
+						var endTime = formatTime( sortedData[ group ][ k ][ 3 ], grouping );
+						var fRate = sortedData[ group ][ k ][ 4 ];
+						var flowDisplay = ( typeof fRate === "number" ) ? fRate.toFixed( 2 ) + " L/min" : "";
 
-                                                groupArray[ i ] += "<tr>" +
-                                                        "<td>" + stationName + "</td>" + // Station name
-                                                        "<td>" + runTime + "</td>" + // Runtime
-                                                        "<td>" + startTime + "</td>" + // Startdate
-                                                        "<td>" + endTime + "</td>" + // Enddate
-                                                        "<td>" + flowDisplay + "</td>" + // Flow rate
-                                                        "</tr>";
+						groupArray[ i ] += "<tr>" +
+							"<td>" + stationName + "</td>" + // Station name
+							"<td>" + runTime + "</td>" + // Runtime
+							"<td>" + startTime + "</td>" + // Startdate
+							"<td>" + endTime + "</td>" + // Enddate
+							"<td>" + flowDisplay + "</td>" + // Flow rate
+							"</tr>";
 					}
 					groupArray[ i ] += "</tbody></table></div>";
 
